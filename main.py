84a940a2df663307e131c62a6daca71115994d9d
Maestro <volodimirkucak996@gmail.com>
2025-09-14 23:13:58 +0300

    Added control points for cubic spline

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+data =[
+  {"elevation": 1663, "longitude": 7.7500, "latitude": 46.0167},
+  {"elevation": 1740, "longitude": 7.7398, "latitude": 46.0126},
+  {"elevation": 1830, "longitude": 7.7296, "latitude": 46.0085},
+  {"elevation": 1940, "longitude": 7.7194, "latitude": 46.0044},
+  {"elevation": 2060, "longitude": 7.7092, "latitude": 46.0003},
+  {"elevation": 2200, "longitude": 7.6990, "latitude": 45.9962},
+  {"elevation": 2350, "longitude": 7.6888, "latitude": 45.9921},
+  {"elevation": 2550, "longitude": 7.6786, "latitude": 45.9880},
+  {"elevation": 2900, "longitude": 7.6684, "latitude": 45.9839},
+  {"elevation": 4181, "longitude": 7.6586, "latitude": 45.9763}
+]
